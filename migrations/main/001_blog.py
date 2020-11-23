78217a55bceecedc0c581be16eff549b61ead1f6
veskor <skorupan.veljko@jjzmaj.edu.rs>
2020-11-23 20:49:26 +0100

    fix migration for blog

--- migrations/main/001_blog.py
+++ migrations/main/001_blog.py
@@ -36,24 +36,6 @@ SQL = pw.SQL
 def migrate(migrator, database, fake=False, **kwargs):
     """Write your migrations here."""
 
-    @migrator.create_model
-    class BaseModel(pw.Model):
-        id = pw.AutoField()
-
-        class Meta:
-            table_name = "basemodel"
-
-    @migrator.create_model
-    class User(pw.Model):
-        id = pw.AutoField()
-        active = pw.BooleanField(constraints=[SQL("DEFAULT False")])
-        admin = pw.BooleanField(constraints=[SQL("DEFAULT False")])
-        confirmed_at = pw.DateTimeField(null=True)
-        email = pw.TextField(unique=True)
-        password = pw.TextField()
-
-        class Meta:
-            table_name = "users"
 
     @migrator.create_model
     class Blog(pw.Model):
@@ -69,35 +51,8 @@ def migrate(migrator, database, fake=False, **kwargs):
         class Meta:
             table_name = "blog"
 
-    @migrator.create_model
-    class Role(pw.Model):
-        id = pw.AutoField()
-        description = pw.TextField(null=True)
-        name = pw.CharField(max_length=255, unique=True)
-
-        class Meta:
-            table_name = "role"
-
-    @migrator.create_model
-    class UserRoles(pw.Model):
-        id = pw.AutoField()
-        role = pw.ForeignKeyField(backref='userroles_set', column_name='role_id', field='id', model=migrator.orm['role'])
-        user = pw.ForeignKeyField(backref='userroles_set', column_name='user_id', field='id', model=migrator.orm['users'])
-
-        class Meta:
-            table_name = "userroles"
-
-
 
 def rollback(migrator, database, fake=False, **kwargs):
     """Write your rollback migrations here."""
 
-    migrator.remove_model('userroles')
-
-    migrator.remove_model('users')
-
-    migrator.remove_model('role')
-
     migrator.remove_model('blog')
-
-    migrator.remove_model('basemodel')
